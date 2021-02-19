96cfd625b354b114bba7e078f94d2091135bda17
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 14:51:32 +0100

    Update Main.java
    changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    // comment
     public static void main(String[] args) {
+        // comment
         System.out.println("this is main branch");
+        // com
+        System.out.println("hello");
+        
+        
+    }
+    
+    public void hello(){
+        // dokoncit   
     }
 }
